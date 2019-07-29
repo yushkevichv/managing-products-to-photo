492a6cd6076d67a787c1823d8fc4d6dce36728a9
Vitaliy Yushkevich <yushkevichv@gmail.com>
2019-07-30 00:44:57 +0300

    clean repo

--- app/Repositories/PhotoContainerRepository.php
+++ app/Repositories/PhotoContainerRepository.php
@@ -12,30 +12,16 @@ class PhotoContainerRepository
     public function getContainersWithTypedProducts() : Collection
     {
         $result = [];
-        $timeStart = microtime(true);
-
-//        $containers = Container::query()
-//            ->select('containers.id as container_id', 'products.type_id')
-//            ->join('product_container', 'containers.id', '=', 'product_container.container_id')
-//            ->join('products', 'products.id', '=', 'product_container.product_id')
-////            ->toSql();
-////        dd($containers);
-//            ->get();
 
         $containers = Container::query()
             ->select('containers.id as container_id', 'container_product_type.product_type_id as type_id')
             ->join('container_product_type', 'containers.id', '=', 'container_product_type.container_id')
-//            ->join('products', 'products.id', '=', 'product_container.product_id')
-//            ->toSql();
-//        dd($containers);
             ->get();
 
         $containers = array_reduce($containers->toArray(), function (array $data, array $element) {
             $data[$element['container_id']][] = $element;
             return $data;
         }, []);
-        echo 'time sql: '.(microtime(true) - $timeStart );
-        $timeStart = microtime(true);
 
         foreach ($containers as $containerId => $container) {
             $result[$containerId]= [
@@ -45,9 +31,7 @@ class PhotoContainerRepository
                 'unique_count' => count(array_unique(array_column($container,'type_id'))),
             ];
         }
-        echo 'time php: '.(microtime(true) - $timeStart );
 
         return collect($result);
     }
-
 }
