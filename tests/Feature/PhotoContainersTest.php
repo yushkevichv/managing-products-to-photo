e4baf1a58c8103d830b35d52d0cddc559b5fffb6
Vitaliy Yushkevich <yushkevichv@gmail.com>
2019-08-01 14:18:28 +0300

    add tests

--- tests/Feature/PhotoContainersTest.php
+++ tests/Feature/PhotoContainersTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\Container;
+use App\Models\Product;
 use App\Models\ProductType;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -11,13 +14,14 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class PhotoContainersTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Test getting optimal containers for photo
      *
      * @return void
      */
-    public function testSuccessfulGetContainers()
+    public function testSuccessfulGetOptimalContainers()
     {
-        $response = $this->get('/api/containers/get');
+//        Artisan::call('db:seed');
+        $response = $this->get('/api/containers/get-optim');
 
         $data = json_decode($response->getContent());
 
@@ -32,5 +36,92 @@ class PhotoContainersTest extends TestCase
         $this->assertEquals($data->count, count($data->data));
 
         $response->assertStatus(200);
+    }
+
+    public function testSuccessGetAllContainers()
+    {
+        $response = $this->get('/api/containers/');
+
+        $response->assertJsonStructure([
+            'data',
+            'links',
+            'meta'
+
+        ]);
+        $response->assertStatus(200);
+    }
+
+    public function testSuccessGetContainer()
+    {
+        $container = factory(Container::class)->create();
+
+        $response = $this->get('/api/containers/'.$container->id);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'products'
+            ],
+        ]);
+        $response->assertStatus(200);
+    }
+
+    public function testGetNonExistContainer()
+    {
+        $response = $this->get('/api/containers/-1');
+
+        $response->assertJsonStructure([
+            'error'
+        ]);
+        $response->assertStatus(404);
+    }
+
+    public function testSuccessDestroyContainer()
+    {
+        $container = factory(Container::class)->create();
+
+        $response = $this->delete('/api/containers/'.$container->id);
+
+        $response->assertJsonStructure([
+            'data'
+        ]);
+        $response->assertStatus(200);
+    }
+
+    public function testDestroyNonExistContainer()
+    {
+        $response = $this->delete('/api/containers/-1');
+
+        $response->assertJsonStructure([
+            'error'
+        ]);
+        $response->assertStatus(404);
+    }
+
+    public function testSuccessCreateContainer()
+    {
+        $products = Product::select('id')->limit(10)->get();
+        $response = $this->post('/api/containers/', [
+            'name' => 'test container',
+            'products' =>$products->toArray()
+        ]);
+
+        $response->assertJsonStructure([
+            'data'
+        ]);
+        $response->assertStatus(201);
+    }
+
+    public function testValidationErrorCreateContainer()
+    {
+        $response = $this->post('/api/containers/', [
+
+        ]);
+
+        $response->assertJsonStructure([
+            'error'
+        ]);
+        $response->assertStatus(422);
     }
 }
